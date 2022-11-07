e222181c2aef28da7c92f40eecf2ad6bdde952db
Lakshika de Zoysa <dezoysalakshika813@gmail.com>
2022-11-07 22:18:05 +0530

    Add siteRouter

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,10 +1,12 @@
 import express from 'express'
 import userRouter from './user'
 import purchaseOrderRouter from './purchaseOrder'
+import siteRouter from './site'
 
 const router = express.Router()
 
 router.use('/users', userRouter)
 router.use('/purchase-order', purchaseOrderRouter)
+router.use('/site', siteRouter)
 
 export default router
