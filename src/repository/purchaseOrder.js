6ac8e59db050e05c56ded0a99b93a68f0b0a0420
Nimtara Munasinghe <73902228+nimtaraMunasinghe@users.noreply.github.com>
2022-11-14 13:15:37 +0530

    Update purchaseOrder.js

--- src/repository/purchaseOrder.js
+++ src/repository/purchaseOrder.js
@@ -1,11 +1,31 @@
 import PurchaseOrder from '../models/purchaseOrder'
-import SiteManager from '../models/siteManager'
 
 export const addPurchaseOrderRepository = async (purchaseOrder) => {
   try {
-    // const { site_manager_id } = purchaseOrder
     const newPurchaseOrder = await new PurchaseOrder(purchaseOrder).save()
     return newPurchaseOrder
+  } catch (error) {
+    return { status: 400, message: error.message }
+  }
+}
+
+export const updatePurchaseOrderRepository = async (purchaseOrderId, purchaseOrder) => {
+  try {
+    const updatedPurchaseOrder = await PurchaseOrder.findByIdAndUpdate(
+      purchaseOrderId,
+      purchaseOrder,
+      { new: true }
+    )
+    return updatedPurchaseOrder
+  } catch (error) {
+    return { status: 400, message: error.message }
+  }
+}
+
+export const deletePurchaseOrderRepository = async (purchaseOrderId) => {
+  try {
+    const deletedPurchaseOrder = await PurchaseOrder.findByIdAndDelete(purchaseOrderId)
+    return deletedPurchaseOrder
   } catch (error) {
     return { status: 400, message: error.message }
   }
@@ -15,6 +35,15 @@ export const getAllPurchaseOrdersRepository = async () => {
   try {
     const purchaseOrders = await PurchaseOrder.find()
     return purchaseOrders
+  } catch (error) {
+    return { status: 400, message: error.message }
+  }
+}
+
+export const getPurchaseOrderByIdRepository = async (purchaseOrderId) => {
+  try {
+    const purchaseOrder = await PurchaseOrder.findById(purchaseOrderId)
+    return purchaseOrder
   } catch (error) {
     return { status: 400, message: error.message }
   }
