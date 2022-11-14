3aa96f9e615e65b82ae9aee146058d8d62f9454d
Nimtara Munasinghe <73902228+nimtaraMunasinghe@users.noreply.github.com>
2022-11-14 18:00:58 +0530

    fixed

--- src/repository/supplier.js
+++ src/repository/supplier.js
@@ -3,7 +3,7 @@ import Supplier from '../models/supplier'
 
 export const getAllSuppliersByCompanyRepository = async (company_id) => {
   try {
-    const { suppliers } = await Company.find({ company_id }).populate('suppliers')s
+    const { suppliers } = await Company.find({ company_id }).populate('suppliers')
     return suppliers
   } catch (error) {
     console.error(error.code, error.message)
